e472452df44be18438b3e4af472658f07c455d78
Russell Hedrick <hedrickrussell@gmail.com>
2023-07-20 12:19:26 -0500

    Add function to determine file extension type.

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,18 @@ fn main() {
     let mut file_name = String::new();
     io::stdin().read_line(&mut file_name).expect("Failed to read line");
     let file_name = file_name.trim().to_owned();
-
+    // Determine File Extension
+    let extension = file_extension(&file_name);
+    match extension {
+        ".txt" => println!("The file extension is .txt"),
+        ".csv" => println!("The file extension is .csv"),
+        ".xlsx" => println!("The file extension is .xlsx"),
+        _ => println!("A valid file was not provided"),
+    }
+    // Read File Contents
     println!("File Name: {file_name}\n\n");
     read_file(file_name);
+
 }
 
 fn read_file(file_name: String) {
@@ -19,5 +28,15 @@ fn read_file(file_name: String) {
     file.read_to_string(&mut contents)
         .expect("Error when attempting to read file.");
 
-    println!("File Contents:\n{contents}");
+    println!("File Contents:\n {contents}");
+}
+
+fn file_extension (file_name: &String) -> &str {
+    let bytes = file_name.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b'.' {
+            return &file_name[i..].trim();
+        }
+    }
+    &file_name
 }
